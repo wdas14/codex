6cd0e4bd95b058f336917b2d8deb16b507cda64d
wdas14 <wdastaunton@gmail.com>
2024-09-13 15:41:07 +0100

    adding initial counter page with some config changes

---
+++ src/app/counter/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <>
+      <Link href="/">Back to homepage</Link>
+      <h1>Hello, Counter page!</h1>
+    </>
+  );
+}
